e4b6a5cdfcce8de447cf02cfd433a0a30fbb8a77
Evgeny Listratkin <evgeny_listratkin@mail.ru>
2022-01-18 17:42:25 +0000

    hhtps

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+aptget update
+apt-get install -y apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
