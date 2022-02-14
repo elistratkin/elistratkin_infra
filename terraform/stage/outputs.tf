a5d5d007ab6f38a0b17f298908915fb011246eb7
Evgeny Listratkin <evgeny_listratkin@mail.ru>
2022-02-14 18:36:56 +0000

    HW09 Ansible-2

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,7 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
 	value = module.db.external_ip_address_db
 }
+output "internal_ip_address_db" {
+	value = module.db.internal_ip_address_db
+}
+
